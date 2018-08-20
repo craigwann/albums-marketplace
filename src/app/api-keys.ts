e0b0346f63f060fa7cd371a8273208bf732c3089
craig wann and Kelli Brooke <craig@>
2018-08-20 16:31:12 -0700

    setup and import sample-albums.json for firebase setup

---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export const masterFirebaseConfig = {
+    apiKey: "AIzaSyAo8eYyPPchNY3HZ5CPxFfFim0ceo4Obck",
+    authDomain: "albums-a5432.firebaseapp.com",
+    databaseURL: "https://albums-a5432.firebaseio.com",
+    storageBucket: "albums-a5432.appspot.com",
+    messagingSenderId: "101735384069"
+  };
