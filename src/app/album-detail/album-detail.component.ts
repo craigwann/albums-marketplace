5880d3ad1ebe7a79911ff22284b3683d1c880a78
craig wann and eric shull <eric@>
2018-08-22 13:20:41 -0700

    add edit and delete crud

--- src/app/album-detail/album-detail.component.ts
+++ src/app/album-detail/album-detail.component.ts
@@ -23,9 +23,15 @@ export class AlbumDetailComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.route.params.forEach((urlParameters) => {
-      this.albumId = urlParameters['id'];
-    });
-    this.albumToDisplay = this.albumService.getAlbumById(this.albumId);
+    this.route.params.forEach((urlParametersArray) => {
+     this.albumId = urlParametersArray['id'];
+   });
+   this.albumService.getAlbumById(this.albumId).subscribe(dataLastEmittedFromObserver => {
+     this.albumToDisplay = new Album(dataLastEmittedFromObserver.title,
+                                      dataLastEmittedFromObserver.artist,
+                                      dataLastEmittedFromObserver.description)
+
+     console.log(this.albumToDisplay);
+   })
   }
 }
